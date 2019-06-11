bc88ebb011101385f78a3d1c4b6480b8aef1a79d
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2019-06-11 21:25:50 +0800

    Update Installed_Information.php

--- include/Installed_Information.php
+++ include/Installed_Information.php
@@ -16,38 +16,38 @@
         }
         private function create_data_array()
         {
-            $hrSWInstalledIndex = array();
-            $hrSWInstalledIndex = snmpwalkoid($this->host_address, "public", ".iso.org.dod.internet.mgmt.mib-2.host.hrSWInstalled.hrSWInstalledTable.hrSWInstalledEntry.hrSWInstalledIndex");
-            if($hrSWInstalledIndex != FALSE)
+        
+        $soft = array();
+		$result = snmpwalk($this->host_address, "public", '.iso.org.dod.internet.mgmt.mib-2.host.hrSWInstalled.hrSWInstalledTable.hrSWInstalledEntry');
+        $soft = array_chunk($result,count($result)/5);
+//            $hrSWInstalledIndex = array();
+//            $hrSWInstalledIndex = snmpwalkoid($this->host_address, "public", ".iso.org.dod.internet.mgmt.mib-2.host.hrSWInstalled.hrSWInstalledTable.hrSWInstalledEntry.hrSWInstalledIndex");
+            if($soft != FALSE)
             {
-                if(count($hrSWInstalledIndex) > 0)
+                if(count($soft) > 0)
                 {
                     $this->data_fetched = 'yes';
                     
-                    $installed_index = array();
+                    $installed_type = array();
                     $installed_name  = array();
                     $installed_date  = array();
-                    $i = 0;
-                    foreach($hrSWInstalledIndex as $key => $value)
+                    for($i=0; $i<count($soft[3]); $i++)
                     {
-                        $installed_index[$i] = str_replace('INTEGER: ', '', $value);
-                        $i = $i + 1;
+                        $installed_type[$i] = str_replace('INTEGER: ', '', $soft[3][$i]);
                     }
-                    $hrSWInstalledIndex = null;
-                    for($i=0; $i<count($installed_index); $i++)
+
+                    for($i=0; $i<count($soft[1]); $i++)
                     {
-                        $data = snmpget($this->host_address, 'public', '.iso.org.dod.internet.mgmt.mib-2.host.hrSWInstalled.hrSWInstalledTable.hrSWInstalledEntry.hrSWInstalledName.'.$installed_index[$i], 300);
-                        $data = str_replace('STRING: ', '', $data);
+                        $data = str_replace('STRING: ', '', $soft[1][$i]);
                         $data = str_replace('"', '', $data);
                         $installed_name[$i] = $data;
                     }
-                    for($i=0; $i<count($installed_index); $i++)
+                    for($i=0; $i<count($soft[4]); $i++)
                     {
-                        $data = snmpget($this->host_address, 'public', '.iso.org.dod.internet.mgmt.mib-2.host.hrSWInstalled.hrSWInstalledTable.hrSWInstalledEntry.hrSWInstalledDate.'.$installed_index[$i], 300);
-                        $data = str_replace('STRING: ', '', $data);
+                        $data = str_replace('STRING: ', '', $soft[4][$i]);
                         $installed_date[$i] = $data;
                     }
-                    $this->data_array[0] = $installed_index;
+                    $this->data_array[0] = $installed_type;
                     $this->data_array[1] = $installed_name;
                     $this->data_array[2] = $installed_date;
                 }
